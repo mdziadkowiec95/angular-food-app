7b7cf093d698fffb311784441733c0e64b7a7bbc
Michal Dziadkowiec <michal.dziadkowiec@motorolasolutions.com>
2020-04-16 20:59:49 +0200

    Cleanup in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,9 @@ import { FoodService } from './services/food.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'angular-food-places';
-  city: any;
-
   constructor(private foodService: FoodService) {}
 
   ngOnInit(): void {
     this.foodService.city.subscribe(city => this.city = city);
-
-    // this.foodService.getCitySuggestions().subscribe(res => console.log('suggestions: ', res))
   }
 }
